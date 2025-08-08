ceed20c7cfa52575279d91e5a9fad5a9972b9ad2
Govindaspkta <sapgovinda05@gmail.com>
2025-08-09 00:01:08 +0545

    post method employee

--- api/views.py
+++ api/views.py
@@ -53,5 +53,10 @@ class EmployeeView(APIView):
         serializer=EmployeeSerializer(employees,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
     
-    def post():
-        pass
+    def post(self,request):
+        serializer=EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_404_BAD_REQUEST)
+        
